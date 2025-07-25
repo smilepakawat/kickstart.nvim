5b67ed8fc01e4c3cde5a7af78d230d1b809fdff1
gg ez mid <smile.pakawat@odds.team>
2025-07-25 17:33:40 +0700

    change tokyo-night theme to bamboo

--- init.lua
+++ init.lua
@@ -876,25 +876,23 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      require('bamboo').setup {
+        colors = {
+          green = '#00ffaa',
+        },
+        highlights = {
+          ['@comment'] = { fg = '$grey', fmt = 'italic' },
+          ['@string'] = { fg = '$green' },
+          ['@function'] = { fmt = 'bold' },
         },
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'bamboo'
     end,
   },
 
